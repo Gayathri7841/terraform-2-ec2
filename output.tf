919f07236ac250f07fa613400b8d76f4c43cf916
Gayathri7841 <boinagayathri1201@gmail.com>
2026-01-01 16:37:59 +0530

    removed files

--- output.tf
+++
@@ -1,4 +0,0 @@
-output "public_ips" {
-  value = aws_instance.ec2[*].public_ip
-}
-
--- variable.tf
+++
@@ -1,8 +0,0 @@
-variable "ami_id" {
-  description = "AMI ID"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-}
-
